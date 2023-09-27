f1a42f37798ebeccc33d2732a4c4031115c9cfe7
Inha Bae <inhabae3@gmail.com>
2023-09-27 11:47:37 -0700

    Update alphabeta()

--- engine.py
+++ engine.py
@@ -1,11 +1,9 @@
-import math
 import chess
 
 class ChessEngine:
-    def __init__(self, depth=2):
+    def __init__(self, depth):
         
         self.board = chess.Board()  
-        self.eval = -math.inf # what to set this as
         self.best_move = None
         self.depth = depth
 
@@ -24,11 +22,9 @@ class ChessEngine:
             50, 50, 50, 50, 50, 50, 50, 50,
             0,  0,  0,  0,  0,  0,  0,  0
         ]
-
         # Black piece-square table does not need to be a deepcopy 
         # since the values will not be modified.
         self.black_pawn_table = self.white_pawn_table[::-1]
-        
         self.white_knight_table = [
             -50, -40, -30, -30, -30, -30, -40, -50, 
             -40, -20, 0, 5, 5, 0, -20, -40, 
@@ -70,7 +66,7 @@ class ChessEngine:
             -5,  0,  5,  5,  5,  5,  0, -5,
             -10,  0,  5,  5,  5,  5,  0,-10,
             -10,  0,  0,  0,  0,  0,  0,-10, 
-            -20,-10,-10, -5, -5,-10,-10,-20,
+            -20,-10,-10, -5, -5,-10,-10,-20
         ]
         self.black_queen_table = self.white_queen_table[::-1]
         self.white_king_table = [
@@ -81,97 +77,85 @@ class ChessEngine:
             -30, -40, -40, -50, -50, -40, -40, -30, 
             -30, -40, -40, -50, -50, -40, -40, -30, 
             -30, -40, -40, -50, -50, -40, -40, -30, 
-            -30, -40, -40, -50, -50, -40, -40, -30, 
+            -30, -40, -40, -50, -50, -40, -40, -30
         ]
         self.black_king_table = self.white_king_table[::-1]
-    # def set_depth(self, depth):
-    #     self.depth = depth
+    def set_depth(self, depth):
+        self.depth = depth
     
-    # TODO: ensure valid fen 
+    # TODO: ensure valid fen ?
     def set_board_with_FEN(self, fen):
         self.board = chess.Board(fen)
     
     # Return board evaluation based on material and positional advantages
     # Evaluation is subjective to the player (+ is winning, - is losing)
+    # Material Evaluation from: https://www.chessprogramming.org/Simplified_Evaluation_Function
     def evaluate(self):
         eval = 0
         for square, piece in self.board.piece_map().items():
             if piece.piece_type == chess.PAWN:
-                eval += self.white_pawn_table[square] if piece.color == chess.WHITE else -self.black_pawn_table[square]
+                if piece.color == chess.WHITE:
+                    eval += (100 + self.white_pawn_table[square])
+                else:
+                    eval -= (100 + self.black_pawn_table[square])
             elif piece.piece_type == chess.KNIGHT:
-                eval += self.white_knight_table[square] if piece.color == chess.WHITE else -self.black_knight_table[square]
+                if piece.color == chess.WHITE:
+                    eval += (320 + self.white_knight_table[square])
+                else:
+                    eval -= (320 + self.black_knight_table[square])
             elif piece.piece_type == chess.BISHOP:
-                eval += self.white_bishop_table[square] if piece.color == chess.WHITE else -self.black_bishop_table[square]
-            elif piece.piece_type == chess.ROOK and piece.color == chess.WHITE:
-                eval += self.white_rook_table[square]
-            elif piece.piece_type == chess.ROOK and piece.color == chess.BLACK:
-                eval -= self.black_rook_table[square]
+                if piece.color == chess.WHITE:
+                    eval += (330 + self.white_bishop_table[square])
+                else:
+                    eval -= (330 + self.black_bishop_table[square])    
+            elif piece.piece_type == chess.ROOK:
+                if piece.color == chess.WHITE:
+                    eval += (500 + self.white_rook_table[square])
+                else:
+                    eval -= (500 + self.black_rook_table[square])
             elif piece.piece_type == chess.QUEEN:
-                eval += self.white_queen_table[square] if piece.color == chess.WHITE else -self.black_queen_table[square]
+                if piece.color == chess.WHITE:
+                    eval += (900 + self.white_queen_table[square])
+                else:
+                    eval -= (900 + self.black_queen_table[square])    
             elif piece.piece_type == chess.KING and piece.color == chess.WHITE:
-                eval += self.white_king_table[square]
+                eval += (self.white_king_table[square] + 20000)
             elif piece.piece_type == chess.KING and piece.color == chess.BLACK:
-                eval -= self.black_king_table[square]
+                eval -= (self.black_king_table[square] + 20000)
         return eval if self.board.turn == chess.WHITE else -eval
 
-    def negamax(self, depth):
-        '''
-        Using negamax, the engine evaluates the position.
-        '''
-        # end of recursion, return the evaluation of the position 
-        self.boards_explored += 1
-        if self.boards_explored % 100 == 0:
-            print(f'# Explored Boards: {self.boards_explored}')
-
-        if self.board.is_game_over() == True:
+    def alphabeta(self, alpha, beta, depth):
+        # check for checkmate/draw
+        if self.board.is_game_over(): 
             if self.board.is_check():
-                return -math.inf
+                return float('-inf')
             return 0 # draw  
+        
+        # TODO: how to check if 3fold rep is good for bad
+        # if self.board.can_claim_threefold_repetition():
+        #     return float('-inf') if self.evaluate() < 0 else 0
 
         if depth == 0:
             return self.evaluate()
-    
-        max_eval = -math.inf
-        for move in self.board.legal_moves:
+            # need quiescence search, change later
+            #return self.quiescence_search(alpha, beta)
+
+        for move in self.board.generate_legal_moves():
+
+            ### Debugging Purposes
+            # if depth == 4:
+            #     print(f"Move {self.board.san(move)} is being considered...")
             self.board.push(move)
-            eval = -self.negamax(depth - 1)
+            eval = -self.alphabeta(-beta, -alpha, depth-1)
             self.board.pop()
-            if eval > max_eval:
-                max_eval = eval
-            if depth == self.depth:
-                self.best_move = move
-        return max_eval
-
-    # def alphabeta(self, depth, alpha=-math.inf, beta=math.inf):
-    #     '''
-    #     Using alpha beta pruning, the engine returns the best move and 
-    #     evaluation of the position by going into a given depth.
-    #     '''
-    #     # might need to check for draws, change later
-    #     if self.board.is_game_over(): 
-    #         if self.board.is_check():
-    #             return -math.inf
-    #         return 0 # draw  
-        
-    #     if self.board.can_claim_threefold_repetition():
-    #         return -math.inf if self.evaluate() < 0 else 0
-
-    #     if depth == 0: # need quiescence search, change later
-    #         return self.quiescence_search(alpha, beta)
-
-    #     moves = self.order_moves(self.board.legal_moves)
-    #     if depth == self.depth:
-    #         self.best_move = moves[0] 
-    #     for move in moves:
-    #         self.board.push(move)
-    #         eval = -self.alphabeta(depth - 1, -beta, -alpha)
-    #         self.board.pop()
-    #         if eval > alpha and depth == self.depth:
-    #             self.best_move = move
-    #         if alpha >= beta:
-    #             return beta 
-    #         alpha = max(alpha, eval)
-    #     return alpha
+            if eval >= beta:
+                return beta
+            if eval > alpha:
+                alpha = eval
+                if depth == self.depth:
+                    self.best_move = move
+                    # print(f"Depth {depth}: Move: {self.board.san(self.best_move)} is best with eval: {eval}")
+        return alpha
 
     # def quiescence_search(self, alpha, beta):
     #     if not self.board.is_check():
--- engine_test.py
+++ engine_test.py
@@ -1,16 +1,14 @@
 import engine as engine
 import chess
+import math
 
+DEPTH = 4
 
-chess_engine = engine.ChessEngine()
+chess_engine = engine.ChessEngine(DEPTH)
 
-# Test evaluate()
+### Test evaluate()
 chess_engine.set_board_with_FEN('8/8/8/8/8/8/8/8 w - - 0 1')
 assert chess_engine.evaluate() == 0
-chess_engine.set_board_with_FEN('4K3/8/8/8/8/8/8/8 w - - 0 1')
-assert chess_engine.evaluate() == -50
-chess_engine.set_board_with_FEN('8/8/8/1K6/8/8/8/8 w - - 0 1')
-assert chess_engine.evaluate() == -40
 chess_engine.set_board_with_FEN('8/ppp2pp1/8/3pp2p/3PP2P/8/PPP2PP1/8 w - - 0 1')
 assert chess_engine.evaluate() == 0
 chess_engine.set_board_with_FEN('2b3n1/ppp2pp1/2n5/1B1pp2p/1b1PP2P/2N5/PPP2PP1/2B3N1 w - - 0 1')
@@ -21,12 +19,50 @@ chess_engine.set_board_with_FEN('4k3/8/8/8/8/8/8/4K3 w - - 0 1')
 assert chess_engine.evaluate() == 0
 # symmetrical board: eval should be 0
 chess_engine.set_board_with_FEN('r1b1k1nr/ppp2pp1/2n5/1B1pp1qp/1b1PP1QP/2N5/PPP2PP1/R1B1K1NR w KQkq - 0 1')
-print(chess_engine.evaluate())
 assert chess_engine.evaluate() == 0
 # symmetrical board except white pawn on h5 and black pawn on h6
 # white gets a +5 and black gets a + 5: eval is 0
 chess_engine.set_board_with_FEN('r1b1k1nr/ppp2pp1/2n4p/1B1pp1qP/1b1PP1Q1/2N5/PPP2PP1/R1B1K1NR w KQkq - 0 1')
 assert chess_engine.evaluate() == 0
 # symmetrical board with queens, eval should be 0
-chess_engine.set_board_with_FEN('qqqqqqqq/qQqQQqQq/qqqqqqqq/8/8/QQQQQQQQ/QqQqqQqQ/QQQQQQQQ w - - 0 1')
+chess_engine.set_board_with_FEN('8/8/qqqqqqqq/8/8/QQQQQQQQ/8/8 w - - 0 1')
 assert chess_engine.evaluate() == 0
+# asymmetrical board with about +600 advantage for blaack
+chess_engine.set_board_with_FEN('1k1q4/8/8/8/8/1N6/8/1K6 w - - 1 1')
+assert chess_engine.evaluate() < -500
+
+### Test alphabeta()
+# mate in 1
+chess_engine.set_board_with_FEN('4k3/2Q5/6N1/8/8/8/8/4K3 w - - 0 1') 
+assert chess_engine.alphabeta(-math.inf, math.inf, 4) == float('inf')
+# mate in 2
+chess_engine.set_board_with_FEN('r1bq2r1/b4pk1/p1pp1p2/1p2pP2/1P2P1PB/3P4/1PPQ2P1/R3K2R w')
+assert chess_engine.alphabeta(-math.inf, math.inf, 4) == math.inf
+# mate in 4
+chess_engine.set_board_with_FEN('r1bq2r1/b4pk1/p1pp1p2/1p2pP2/1P2P1PB/3P4/1PPQ2P1/R3K2R w')
+assert chess_engine.alphabeta(-math.inf, math.inf, 4) == math.inf
+# mated in 1
+chess_engine.set_board_with_FEN('8/8/8/8/8/5rk1/8/6K1 w - - 0 1')
+assert chess_engine.alphabeta(-math.inf, math.inf, 4) == float('-inf')
+# mated in 2
+chess_engine.set_board_with_FEN('7k/8/8/8/2q5/1r6/7K/8 w - - 0 1')
+assert chess_engine.alphabeta(-math.inf, math.inf, 4) == float('-inf')
+
+# find good move
+print("Find good move (1):")
+chess_engine.set_board_with_FEN('1k1q4/8/8/N7/8/8/8/1K6 w - - 0 1')
+chess_engine.alphabeta(-math.inf, math.inf, 4)
+print(chess_engine.board.san(chess_engine.best_move))
+# find good move
+print("Find good move (2):")
+chess_engine.set_board_with_FEN('r1bqk1nr/pppp1ppp/2n5/4p1N1/1bB1P3/8/PPPP1PPP/RNBQK2R w KQkq - 0 1')
+chess_engine.alphabeta(-math.inf, math.inf, 4)
+print(chess_engine.board.san(chess_engine.best_move))
+
+
+
+
+
+
+
+
